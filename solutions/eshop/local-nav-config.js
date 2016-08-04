bb66e2efbdb36aab77bb318cbd9008c15e05392b
Jakub Černý <kuba@persoo.cz>
2016-08-04 02:29:12 +0200

    Adding rendered data.

---
+++ solutions/eshop/local-nav-config.js
@@ -0,0 +1,8 @@
+var persooDemo = persooDemo || {};
+
+persooDemo.localNavigation = {
+	'Homepage': 'index.html',	
+	'Category': 'category.html',
+	'Detail': 'detail.html',
+	'Basket': 'basket.html'
+};
