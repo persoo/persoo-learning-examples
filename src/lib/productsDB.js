c7ea776a9aca0e3b3409a6bd2101021b6a008e30
Michal Fryč <michal.fryc@seznam.cz>
2016-09-09 13:43:01 +0200

    Další várka změn UX
    Další várka změn UX

--- src/lib/productsDB.js
+++ src/lib/productsDB.js
@@ -88,7 +88,7 @@ function showProductDetail(data) {
 	html += '</div>';
 	html += '<div class="boxDebug">';
 	html += '	 <h2 class="more-info">ITEM as JSON <div class="buttons"><span class="link" onclick="showDebug()">show/hide</span><span class="link" onclick="foldDebug()">fold</span></div></h2>';
-	html += '  <div class="editor debug hide" id="jsoneditor">' + JSON.stringify(item) + '</div>';
+	html += '  <div class="editor debug hide" id="jsoneditor"><pre>' + JSON.stringify(item) + '</pre></div>';
 	html += '</div>';
 	html += '<div id="box-alsoBoughtProducts" class="mini-products"><h2>Also Bought</h2><div id="alsoBoughtProducts"></div><div class="clear"></div></div>';
 	html += '<div id="box-alsoViewedProducts" class="mini-products"><h2>Also Viewed</h2><div id="alsoViewedProducts"></div><div class="clear"></div></div>';
