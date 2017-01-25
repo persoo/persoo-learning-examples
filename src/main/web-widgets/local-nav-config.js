c07634d7d2cccbc918807c48c21e73e2e8a4cd83
Jakub Černý <kuba@persoo.cz>
2017-01-25 01:29:09 +0100

    TASK - adding more demos for web widgets

--- src/main/web-widgets/local-nav-config.js
+++ src/main/web-widgets/local-nav-config.js
@@ -1,5 +1,7 @@
 var persooDemo = persooDemo || {};
 
 persooDemo.localNavigation = {
-    'Pop-ups': 'index.html'
+    'Pop-ups': 'index.html',
+    'Infobars': 'infobar.html',
+    'Side boxes': 'sidebox.html'
 };
